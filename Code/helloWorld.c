2e452226d756b04d6e95c66e2480223efa6026fb
Nitin Malik <nitinmalik02015@gmail.com>
2021-06-16 14:59:29 +0530

    Following Changes are done: 	new file:   addition.c 		added some files 	modified:   helloWorld.c 		added new hello() function

--- Code/helloWorld.c
+++ Code/helloWorld.c
@@ -1,6 +1,12 @@
+// Hello world program
 
 #include <stdio.h>
 
 int main(){
 	printf("Hello World! I am using Linux vim editor")
+	hello();
+}
+void hello(){
+	printf("World is Awesome!")
+	printf("OK ok world")
 }
