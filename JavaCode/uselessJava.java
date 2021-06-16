266d315fca119becfba0aebee7f2252db7d48831
Nitin Malik <nitinmalik02015@gmail.com>
2021-06-16 21:41:21 +0530

    Java code added  Changes to be committed:

--- JavaCode/uselessJava.java
+++ JavaCode/uselessJava.java
@@ -1,8 +1,10 @@
 
+//Java Program
 
 public class uselessJava{
 	public static void main(String[] args){
 		System.out.println("Hello this is uselessJava.java file");
+		System.out.println("Hello another line!!!");
 	}
 }
 
