8578543a7b4d104945a7004e9072c6823c9610ec
Nitin Malik <nitinmalik02015@gmail.com>
2021-06-16 21:48:19 +0530

    some thing delted in java useless  Changes to be committed: 	modified:   JavaCode/uselessJava.java

--- JavaCode/uselessJava.java
+++ JavaCode/uselessJava.java
@@ -1,10 +1,9 @@
 
-//Java Program
 
 public class uselessJava{
 	public static void main(String[] args){
-		System.out.println("Hello this is uselessJava.java file");
 		System.out.println("Hello another line!!!");
 	}
 }
 
+//Java Program
