f00630a1375e93050d63b12f779f09e3768b362d
Nitin Malik <nitinmalik02015@gmail.com>
2021-06-16 15:36:14 +0530

    Following Changes are done: 	new file:   .gitignore 		added new file .gitignore for user purpose 	new file:   Code/Hello.java 		added a new java code Helloworld 	modified:   Code/helloWorld.c 		rectified it a bit

---
+++ Code/Hello.java
@@ -0,0 +1,7 @@
+
+// Java program
+public class Hello{
+	public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
--- Code/helloWorld.c
+++ Code/helloWorld.c
@@ -2,11 +2,12 @@
 
 #include <stdio.h>
 
+void hello();
 int main(){
-	printf("Hello World! I am using Linux vim editor")
+	printf("Hello World! I am using Linux vim editor");
 	hello();
 }
 void hello(){
-	printf("World is Awesome!")
-	printf("OK ok world")
+	printf("World is Awesome!");
+	printf("OK ok world\n");
 }
