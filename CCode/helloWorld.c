40f757863e919bed019a8d273bedeaa2a4d5cc63
Nitin Malik <84228201+nitinmalik00@users.noreply.github.com>
2021-06-17 11:51:24 +0530

    Update helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,13 +1,16 @@
 // Hello world program
 
 #include <stdio.h>
-
+void greeting();
 void hello();
 int main(){
 	printf("Hello World! I am using Linux vim editor");
-	hello();
+	hello();}
+void greeting(){
+	printf("Hello...Greetings");
 }
 void hello(){
 	printf("World is Awesome!");
 	printf("OK ok world\n");
+	greeting();
 }
